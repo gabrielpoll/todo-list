f94c738088ed9e3846c30de78c294a957144d436
gabrielpoll <gabrieldarosa.poll@gmail.com>
2026-02-10 20:10:09 -0300

    create models folder

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./style.css";
+import "./models/Todo.js";
+
+
---
+++ src/models/Todo.js
@@ -0,0 +1,28 @@
+class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.completed = false;
+    }
+
+    toggleCompleted() {
+        const toggle = this.completed ? false : true;
+        this.completed = toggle;
+    }
+
+    update(data) {
+        this.description = data;
+    }
+
+    getSummary() {
+        const status = this.completed ? `Concluido` : `Em progresso`;
+        return `Title: ${this.title} | 
+        Description ${this.description} | 
+        Date ${this.dueDate} | Priority: ${this.priority} | 
+        Status: ${status}`
+    }
+}
+
+export default Todo;
